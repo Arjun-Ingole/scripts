d64bd03fe57503b50517631fac652f5786b95cd9
Arjun-Ingole <arjun@project404.us>
2021-11-22 22:43:24 +0530

    MergeTag: Make Kernel version user selectable
    Plus some cosmetic changes
    
    Signed-off-by: Arjun-Ingole <arjun@project404.us>

--- merge-tags.sh
+++ merge-tags.sh
@@ -4,13 +4,14 @@
 #
 
 read -p "Please Input Tag/Branch to Merge: " TAG
+read -p "Please Enter your kernel version followed by Patch level eg (4.14, 4.19) : " VERSION
 
 # Merge
-echo "Starting Merging"
+echo "<===========< Starting To Merge >===========>"
       git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $TAG && git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
       git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $TAG && git merge -X subtree=drivers/staging/fw-api FETCH_HEAD
       git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $TAG && git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD
       git fetch https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $TAG && git merge -X subtree=techpack/audio FETCH_HEAD
-      git fetch https://source.codeaurora.org/quic/la/kernel/msm-4.14 $TAG && git merge FETCH_HEAD
+      git fetch https://source.codeaurora.org/quic/la/kernel/msm-$VERSION $TAG && git merge FETCH_HEAD
 
 echo "Merge Was Successful"
